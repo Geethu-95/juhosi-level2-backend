01b3323052a95c5801f507c6614c362e06146280
Geethika S <geethu.s1995@gmail.com>
2023-06-30 20:10:33 +0530

    Enabling cors 0.3

--- functions/index.js
+++ functions/index.js
@@ -24,14 +24,14 @@ con.connect(function (err) {
 
 // app.use(cors());
 // app.options('*', cors())
-app.use(cors({
+app.use(cors({origin: '*',
   methods: ['POST']
 }));
 // app.use(express.json())
-const corsOptions = {
-  // origin: "https://juhosi-level2-frontend.web.app"
-  origin: "https://master--deluxe-daffodil-504ed3.netlify.app"
-};
+// const corsOptions = {
+//   // origin: "https://juhosi-level2-frontend.web.app"
+//   origin: "https://master--deluxe-daffodil-504ed3.netlify.app"
+// };
 app.use(bodyParser.urlencoded({
   extended: true
 }));
